9fc95d0428ed5bb99f0eb27cd5d83291c9a88868
Vadim Bondarev <haghard84@gmail.com>
2016-02-29 10:50:01 +0300

    add Hmap example

---
+++ src/main/scala/io/scalac/hmap/Hmap.scala
@@ -0,0 +1,26 @@
+package io.scalac.hmap
+
+object Hmap {
+
+  class GenericType[T](value: T)
+  class GMap[K,V]
+
+  case class GInt(k: Int) extends GenericType[Int](k)
+  case class GString(k: String) extends GenericType[String](k)
+
+  implicit object int2String extends GMap[GInt, GString]
+  implicit object string2Int extends GMap[GString, GInt]
+
+  def main(args: Array[String]) = {
+
+    val hoMap = shapeless.HMap[GMap](
+      GInt(0) -> GString("a"),
+      GInt(99) -> GString("b"),
+      GString("key-a") -> GInt(12)
+    )
+
+    println(hoMap.get(GInt(0)).get)
+    println(hoMap.get(GString("key-a")).get)
+    println(hoMap.get(GInt(99)).get)
+  }
+}
